b1ba5a733d3ba01a80dfeaef91fd4edf6186642a
DanielNunez04 <130614771+DanielNunez04@users.noreply.github.com>
2023-06-28 00:34:57 +0000

    Logica

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,21 +4,13 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
+	// console.log("Hola")
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<p>{props.centenas}</p>
+			<p>{props.decimal}</p>
+			<p>{props.unidad}</p>
 		</div>
 	);
 };
--- src/js/index.js
+++ src/js/index.js
@@ -9,4 +9,20 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+// ReactDOM.render(<Home />, document.querySelector("#app"));
+
+let decimal = 0
+let unidad = 0
+let centenas = 0
+
+setInterval(() => {
+ unidad ++
+ if(unidad >= 9) {
+    unidad = 0
+    decimal ++
+ }
+ if(decimal >= 9) {
+    decimal = 0
+    centenas ++
+ }
+    ReactDOM.render(<Home unidad={unidad} decimal={decimal} centenas={centenas} />, document.querySelector("#app"));} , 1000)
